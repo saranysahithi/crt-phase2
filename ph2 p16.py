3d56945ae5380a156aeaf9687f487181599bfb64
saranyasahithi <164603450+saranysahithi@users.noreply.github.com>
2024-05-29 10:15:36 +0530

    Add files via upload

---
+++ ph2 p16.py
@@ -0,0 +1,14 @@
+def performbubblesort(nums):
+    n=len(nums)
+    fixthisindex=n-1
+
+    while fixthisindex>0:
+        for index in range(fixthisindex):
+            if nums[index]>nums[index+1]:
+                temp=nums[index]
+                nums[index]=nums[index+1]
+                nums[index+1]=temp
+        print(nums)
+        fixthisindex=-1
+nums=[10,8,2,14,12,7]
+print('before sorting:',nums)
