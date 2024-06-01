4a25d30d3ef3d558ceaf0436335c7149f24e5681
saranyasahithi <164603450+saranysahithi@users.noreply.github.com>
2024-06-01 15:02:07 +0530

    Add files via upload

---
+++ User.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self):
+        self.fullName = None 
+        self.mobileNumber = None 
+        self.pin = None 
+        self.defaultBankAccount = None
+        self.previousTransactions = []
+        self.linkedBankAccounts = []
+ 
+ 
